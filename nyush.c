39c6f153a04d41ceeb1dd04a97f85c0522fc08d7
iltenahmet <iltenahmet@gmail.com>
2023-02-26 17:34:34 -0500

    Fix memory leaks

--- nyush.c
+++ nyush.c
@@ -1,5 +1,5 @@
-//nyuc  -- for valgrind: valgrind --leak-check=full --track-origins=yes ./nyush
-//docker command for windows: docker run -i --name cs202 --privileged --rm -t -v C:\Users\ahmet\cs202\labs:/cs202 -w /cs202 ytang/os bash
+// nyuc  -- for valgrind: valgrind --leak-check=full --track-origins=yes ./nyush
+// docker command for windows: docker run -i --name cs202 --privileged --rm -t -v C:\Users\ahmet\cs202\labs:/cs202 -w /cs202 ytang/os bash
 
 #include "stdio.h"
 #include "stdlib.h"
@@ -11,47 +11,44 @@
 
 int main()
 {
-	while(1)
+
+	size_t inputSize = 100;
+	char *input = (char *)malloc(inputSize * sizeof(char));
+
+	int inputArraySize = 7;
+	char **inputArray = malloc(inputArraySize * sizeof(char *));
+
+	while (1)
 	{
-		size_t inputSize = 100;
-		char* input = (char *)malloc(inputSize * sizeof(char));
 
 		printPrompt();
 		if (getUserInput(input, inputSize) != EXIT_SUCCESS)
 		{
 			free(input);
+			free(inputArray);	
 			exit(0);
-		}	
+		}
 
-		int inputArraySize = 7;
-		char** inputArray = malloc(inputArraySize * sizeof(char *));
 		splitStringBySpace(input, &inputArray, &inputArraySize);
 
-		for (int i = 0; i < inputArraySize; i++)
-		{
-			if (inputArray[i] != NULL)
-			{
-			printf("[%s]", inputArray[i]);
-			fflush(stdout);
-			}
-	}
-
-		char* builtInCommand = getBuiltInCommand(input);
+		char *builtInCommand = getBuiltInCommand(inputArray[0]);
 		if (builtInCommand)
 		{
 			printf("Built in: %s", builtInCommand);
 			if (strcmp(builtInCommand, "exit") == 0)
 			{
 				free(input);
+				free(inputArray);	
 				exit(0);
 			}
-			
-		} 
-		else 
+		}
+		else
 		{
 			printf("Not built-in");
 		}
-	}	
+	}
+
+
 }
 
 void printPrompt()
@@ -67,37 +64,37 @@ void printPrompt()
 }
 
 // https://c-for-dummies.com/blog/?p=1112
-int getUserInput(char* buffer, size_t bufsize)
+int getUserInput(char *buffer, size_t bufsize)
 {
 	if (getline(&buffer, &bufsize, stdin) == -1)
 	{
 		if (feof(stdin))
 		{
 			// the user has pressed Ctrl+D
-			return EXIT_FAILURE; 
-		} 
+			return EXIT_FAILURE;
+		}
 		else
 		{
 			perror("Error reading input with getLine()");
 			return EXIT_FAILURE;
-		} 
+		}
 	}
 
-	//change \n at the end of the buffer to \0 so it's a null terminated string
+	// change \n at the end of the buffer to \0 so it's a null terminated string
 	buffer[strcspn(buffer, "\n")] = '\0';
 
 	return EXIT_SUCCESS;
 }
 
-void splitStringBySpace(char *inputString, char ***outputArrayPtr,  int *arraySize) 
+void splitStringBySpace(char *inputString, char ***outputArrayPtr, int *arraySize)
 {
 	char **outputArray = *outputArrayPtr;
 	int tokenCount = 0;
-	char* token = strtok(inputString, " ");
+	char *token = strtok(inputString, " ");
 
-    while (token != NULL) 
+	while (token != NULL)
 	{
-		if (tokenCount == *arraySize) 
+		if (tokenCount == *arraySize)
 		{
 			*arraySize *= 2;
 			outputArray = realloc(outputArray, sizeof(char *) * *arraySize);
@@ -107,19 +104,21 @@ void splitStringBySpace(char *inputString, char ***outputArrayPtr,  int *arraySi
 		outputArray[tokenCount] = token;
 		token = strtok(NULL, " ");
 		tokenCount++;
-    }
+	}
 }
 
-char* getBuiltInCommand(char* command)
+char *getBuiltInCommand(char *command)
 {
-	char* builtInCommands[] = {"cd", "jobs", "fg", "exit"};
-	int count = sizeof(builtInCommands) / sizeof(char*);
+	char *builtInCommands[] = {"cd", "jobs", "fg", "exit"};
+	int count = sizeof(builtInCommands) / sizeof(char *);
 
-    for (int i = 0; i < count; i++) {
-        if (strcmp(command, builtInCommands[i]) == 0) {
-            return builtInCommands[i];
-        }
-    }
+	for (int i = 0; i < count; i++)
+	{
+		if (strcmp(command, builtInCommands[i]) == 0)
+		{
+			return builtInCommands[i];
+		}
+	}
 
-    return NULL;
+	return NULL;
 }
--- nyush.h
+++ nyush.h
@@ -1,4 +1,5 @@
 void printPrompt();
 int getUserInput(char* buffer, size_t bufsize);
 char* getBuiltInCommand(char* command);
-void splitStringBySpace(char *inputString, char ***outputArrayPtr,  int *arraySize); 
+void splitStringBySpace(char *inputString, char ***outputArrayPtr,  int *arraySize);
+void freeStringArray(char ***stringArrayPtr, int size); 
